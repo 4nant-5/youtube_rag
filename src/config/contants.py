5fcd064be5967ee0a3a0f83da02d3fe9f9904184
Anant <anantsingh.hsrah@gmail.com>
2026-07-09 09:27:45 +0530

    Initialize project structure and configuration layer

---
+++ src/config/contants.py
@@ -0,0 +1,15 @@
+APP_NAME = "YouTube Multimodal RAG"
+
+VERSION = "1.0.0"
+
+SUPPORTED_VIDEO_FORMATS = [
+    ".mp4",
+    ".mkv",
+    ".avi"
+]
+
+SUPPORTED_IMAGE_FORMATS = [
+    ".jpg",
+    ".jpeg",
+    ".png"
+]
---
+++ src/config/settings.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+# Project Paths
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+DATA_DIR = PROJECT_ROOT / "data"
+VIDEOS_DIR = DATA_DIR / "videos"
+TRANSCRIPTS_DIR = DATA_DIR / "transcripts"
+FRAMES_DIR = DATA_DIR / "frames"
+CHROMA_DB_DIR = DATA_DIR / "chroma_db"
+
+# Models
+EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
+RERANKER_MODEL = "BAAI/bge-reranker-large"
+LLM_MODEL = "Qwen/Qwen2.5-7B-Instruct"
+VISION_MODEL = "Qwen/Qwen2.5-VL-7B-Instruct"
+
+# Chunking
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
+# Video Processing
+FRAME_INTERVAL = 5
+# Retrieval
+TOP_K = 10
+FINAL_K = 5
